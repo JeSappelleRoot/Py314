1b1c78f500c172009c07fc2df6c7b5bbcabc7989
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-12-15 15:07:52 -0600

    Modify Listener name to Handler

--- listeners.py
+++ listeners.py
@@ -8,11 +8,19 @@ from cmd import Cmd
 from termcolor import colored
 from prettytable import PrettyTable
 
-class ListenerPrompt(Cmd):
-    """Prompt use to define listener and options"""
 
-    prompt = colored('(listener) > ', 'yellow')
-                
+class Handler():
+    """Class to define a handler"""
+    def __init__(self):
+        self.type = 'bind_tcp'
+        self.rhost = ''
+        self.rport = 1234
+
+
+
+
+class HandlerPrompt(Cmd):
+    """Prompt use to define handler and options"""
 
     def do_exit(self, arg):
         """Quit Py314"""
@@ -22,52 +30,16 @@ class ListenerPrompt(Cmd):
         """Return to Py314 main prompt"""
         return True
 
+    def do_settings(self, arg):
+        """Show listener settings"""
 
-    
-
-
-
-
-
-class Py314Bind():
-    """Simple Py314 Bind TCP handler"""
-    
-    def __init__(self):
-        self.rhost = ''
-        self.rport = 1234
-
-    def get_names(self):
-        return dir(self.__class__)
-
-    def launch(self):
-       
-        launchModule(self.rhost, self.rport)
+    def do_show(self, arg):
 
 
-    def set_rhost(self):
-        """Define IP address of remote host to bind"""
 
-    def set_rport(self):
-        """Define port of remote host to bind"""
 
-    def showOptions(self):
-        """Display options about PolyMolyBindTcp"""
+def startListeners():
 
-        print("""The polymoly_bind_tcp backdoor is designed to bind a specified host with IP & port combination.""")
-        print("""If the connection is successfull, a shell is automatically give, and remote actions are allowed""")
-
-        table = PrettyTable()
-        headers = ['name', 'value', 'description']
-        table.field_names = headers
-        for header in headers:
-            table.align[header] = 'l'
-
-        for option in self.__dict__.keys():
-            optionName = option
-            optionValue = self.__dict__[option]
-            optionDoc = getattr(self, f"set_{option}").__doc__
-
-            table.add_row([optionName, optionValue, optionDoc])
-
-        print(table)
-
+    subPrompt = HandlerPrompt()
+    subPrompt.prompt = colored('(handler) > ', 'yellow')
+    subPrompt.cmdloop()
