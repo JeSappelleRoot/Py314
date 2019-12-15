d76223d01d4bd3743c8b95cb036e7c09d623cd44
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-12-15 13:53:59 -0600

    Add terminal with Cmd class

---
+++ Py314.py
@@ -0,0 +1,96 @@
+import re
+import os
+import sys
+import glob
+from cmd import Cmd
+from termcolor import colored
+
+
+
+class Prompt(Cmd):
+
+    
+    prompt = 'Py314 > '
+    intro = 'Type help or ? to list commands.'
+    tempList = []
+    focus = ''
+
+
+    def do_exit(self, arg):
+        """Quit Polymole"""
+        return True
+
+    def do_clear(self, arg):
+        """Clear terminal (shortcut : ctrl-l)"""
+        os.system('clear')
+
+    def do_banner(self, arg):
+        """Clear console and display banner"""
+        os.system('clear')
+        displayBanner()
+
+
+    def do_use(self, arg):
+        """Use module/listener/generater"""
+        # if 'use' with number
+        # Remove '-' to match with negatives numbers 
+        if str(arg).lstrip('-').isdigit():
+            # If module index is not between 0 and tempList lengh - 1
+            if not 0 <= int(arg) <= len(self.tempList) - 1:
+                self.emptyline
+            else:
+                moduleName = str(self.tempList[int(arg)][1])
+                self.focus = moduleName
+                self.focusObject = self.import_focus(self.focus)
+
+        # Else if 'use' with module name
+        else:
+            if arg not in dict(self.tempList).values():
+                print('[!] Invalid module name')
+            else:
+                self.focus = arg
+                self.focusObject = self.import_focus(self.focus)
+
+        self.focusObject.startModule(moduleName)
+
+    def do_help(self, arg):
+        sep = '='
+        headers = ['name', 'description']
+        sepList = []
+        for item in headers:
+            sepList.append(sep * len(item))
+        print('\t\t'.join(headers))
+        print('\t\t'.join(sepList))
+
+        if arg:
+            doc = getattr(self, f"do_{arg}").__doc__
+            if doc:
+                print(f"{arg}\t\t{doc}")
+            else:
+                print(f"[!] No documentation for {arg} command")
+                
+        else:
+            commands = self.get_names()
+            for cmd in commands:
+                if cmd.startswith('do_'):
+                    doc = getattr(self, cmd).__doc__
+                    if doc:
+                        print(f"{cmd.replace('do_', '')} \t\t{doc}")
+
+
+
+
+    pass
+
+
+
+
+displayBanner()
+
+pmCmd = Prompt()
+pmCmd.cmdloop()
+
+
+
+# https://code-maven.com/interactive-shell-with-cmd-in-python
+# https://wiki.python.org/moin/CmdModule
