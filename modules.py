ac826a102a28f8ef7dc43be411550e01ecaf77c9
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-12-17 12:34:51 -0600

    Simplify Shell module, no input reading, default method of Cmd class is override to send non-commands in Shell module

--- modules.py
+++ modules.py
@@ -1,37 +1,24 @@
-def shell(channel):
+def Shell(channel, command):
+    """Try to interact with remote agent with a shell"""
 # Module to send a shell command to remote agent
 
     # Define a buffer size, 1024 bytes
     bufferSize = 1024
 
-    try:
-        # Read input command, with encode to convert string to bytes
-        command = input(f"shell > ").encode()
-        # If user want to clean console
-        if command == 'clear':
-            system('clear')
-        # Else if command is empty, do nothing
-        elif command == b'':
-            pass
-        else:
-            # Else, send command to remote agent
-            channel.sendall(command)
+    if command == b'':
+        pass
+    else:
+        # Else, send command to remote agent
+        channel.sendall(command.encode())
 
-            # While True, receive data
-            while True:
-                rawResponse = channel.recv(bufferSize)
-                # If all data are smaller than the buffer size, break While loop
-                if len(rawResponse) < bufferSize:
-                    break
-            # Decode bytes to string to read the answer of the remote agent
-            shellReponse = rawResponse.decode()
-            # Print agent's answer
-            print(shellReponse)
+        # While True, receive data
+        while True:
+            rawResponse = channel.recv(bufferSize)
+            # If all data are smaller than the buffer size, break While loop
+            if len(rawResponse) < bufferSize:
+                break
+        # Decode bytes to string to read the answer of the remote agent
+        shellReponse = rawResponse.decode()
+        # Print agent's answer
+        print(shellReponse)
 
-    except ConnectionResetError:
-        print(f"\n[-] Channel reset by peer")
-        return
-
-    except BrokenPipeError:
-        print(f"\n[-] Channel reset by peer (broken pipe)")
-        return
