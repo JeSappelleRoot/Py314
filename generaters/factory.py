598c445c97912679098e7a4b2cd50d507351428b
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-12-18 15:07:37 -0600

    Add options of agent factory IP, port, password...

--- generaters/factory.py
+++ generaters/factory.py
@@ -1,6 +1,7 @@
 
 from cmd import Cmd
 from termcolor import colored
+from prettytable import PrettyTable
 
 class Prompt(Cmd):
     """Simple Py314 agent factory"""
@@ -9,11 +10,10 @@ class Prompt(Cmd):
 
     optionsDict = {
         'type': '',
-        'rhost': '',
-        'rport': 1234,
+        'address': '',
+        'port': 1234,
         'password': 'Py314!',
-        'proxy': '', 
-        'verbose': False
+        'proxy': ''
     }
 
 
@@ -36,6 +36,45 @@ class Prompt(Cmd):
         """Quit Py314"""
         exit()
 
+    def do_options(self, arg):
+        """Show currents settings of handler"""
+
+        print("""\nThe bind_tcp handler is designed to bind a specified host with IP & port combination.""")
+        print("""If the connection is successfull, a shell is automatically give, and remote actions are allowed\n""")
+
+        table = PrettyTable()
+        headers = ['name', 'value', 'description']
+        table.field_names = headers
+        for header in headers:
+            table.align[header] = 'l'
+
+        for option in dir(self):
+            if option.startswith('set_'):
+                optionName = option.replace('set_', '')
+                optionValue = self.optionsDict[optionName]
+                optionDoc = getattr(self, option).__doc__
+
+                table.add_row([optionName, optionValue, optionDoc])
+
+        print(table)
+
+
+# < -------------------------- factory OPTIONS -------------------------- >
+
+    def set_type(self):
+        """Define the type of agent : bind_agent/reverse_listener"""
+
+    def set_address(self):
+        """Define the IP address to bind/listen"""
+
+    def set_port(self):
+        """Define the port of Py314 agent to bind/listen"""
+
+    def set_password(self):
+        """Define a password use to connect to Py314 agent and to perform symetric encryption of traffic"""
+
+    def set_proxy(self):
+        """Define a proxy, which be use to bind Py314 agent : <type>://<ip>:<port>"""
 
 
 
