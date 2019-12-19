70d6c85f2a226a2ee66f450a033028edfe1a7f58
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-12-19 15:11:14 -0600

    Add logger module to display informations in Shell module

--- modules.py
+++ modules.py
@@ -1,9 +1,12 @@
 
 import crypto
+import logging
 
 def Shell(channel, password, command):
     """Try to interact with remote agent with a shell"""
 # Module to send a shell command to remote agent
+
+    logger = logging.getLogger('main')
 
     #crypto = Crypto(password)
 
@@ -14,21 +17,26 @@ def Shell(channel, password, command):
         pass
     else:
         # Else, send command to remote agent
+        logger.debug(f'Shell command before encryption : {command}')
         command = crypto.encrypt_message(password, command)
+        logger.debug(f'Shell command after encryption : {command}')
         channel.sendall(command.encode())
 
         # While True, receive data
         while True:
-            print('recv...')
+            logger.debug('recv from socket...')
             rawResponse = channel.recv(bufferSize)
-            print(rawResponse)
+            logger.debug(f'Partial answer : {rawResponse}')
             # If all data are smaller than the buffer size, break While loop
             if len(rawResponse) < bufferSize:
-                print('break !')
+                logger.debug('break recv while loop')
                 break
         # Decode bytes to string to read the answer of the remote agent
+        
         shellReponseEncrypted = rawResponse.decode()
         shellResponse = crypto.decrypt_message(password, shellReponseEncrypted)
+        logger.debug(f'Encrypted answer : {shellReponseEncrypted}')
+        logger.debug(f'Decrypted answer : {shellResponse}')
         # Print agent's answer
         print(shellResponse)
 
