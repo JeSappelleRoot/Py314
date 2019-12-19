9dafff1cfe038b94f414e0feb8abb3fc1269ea00
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-12-19 13:46:35 -0600

    Add some debug prints in Shell module

--- modules.py
+++ modules.py
@@ -1,4 +1,5 @@
-from crypto import Crypto
+
+import crypto
 
 def Shell(channel, password, command):
     """Try to interact with remote agent with a shell"""
@@ -7,22 +8,27 @@ def Shell(channel, password, command):
     #crypto = Crypto(password)
 
     # Define a buffer size, 1024 bytes
-    bufferSize = 1024
+    bufferSize = 4096
 
     if command == b'':
         pass
     else:
         # Else, send command to remote agent
+        command = crypto.encrypt_message(password, command)
         channel.sendall(command.encode())
 
         # While True, receive data
         while True:
+            print('recv...')
             rawResponse = channel.recv(bufferSize)
+            print(rawResponse)
             # If all data are smaller than the buffer size, break While loop
             if len(rawResponse) < bufferSize:
+                print('break !')
                 break
         # Decode bytes to string to read the answer of the remote agent
-        shellReponse = rawResponse.decode()
+        shellReponseEncrypted = rawResponse.decode()
+        shellResponse = crypto.decrypt_message(password, shellReponseEncrypted)
         # Print agent's answer
-        print(shellReponse)
+        print(shellResponse)
 
