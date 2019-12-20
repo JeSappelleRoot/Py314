310ced72f0a1a69d91cde5ccbcb735fab5a3dfcf
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-12-20 15:38:00 -0600

    Add idea : set a different formatter for debug mode, with module name and function name in header

--- core/logger.py
+++ core/logger.py
@@ -8,6 +8,8 @@ def setup_logger(name):
 
     level = logging.INFO
 
+    # Add [from %(funcName)s in %(module)s] in debug level (specify a formatter for differents level)
+
     formatter = logging.Formatter('[%(asctime)s]-[%(levelname)s] : %(message)s', datefmt='%H:%M:%S')
 
     logger = logging.getLogger(name)
