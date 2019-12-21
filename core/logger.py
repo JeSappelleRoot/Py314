1345a2b8bb09547aba4ce33546ddc4a205e72982
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-12-21 15:26:40 -0600

    Add specific format for log level : more info in debug mode

--- core/logger.py
+++ core/logger.py
@@ -2,7 +2,27 @@ import sys
 import coloredlogs
 import logging
 
+class CustomFormatter(logging.Formatter):
+    """Logging Formatter to add colors and count warning / errors"""
 
+    timeFormat = '%H:%M:%S'
+    defaultFormat = '[%(asctime)s]-[%(levelname)s] : %(message)s'
+    debugFormat = '[%(asctime)s]-[%(levelname)s] (from %(funcName)s in %(module)s) : %(message)s'
+    
+
+    FORMATS = {
+        logging.DEBUG: debugFormat,
+        logging.INFO: defaultFormat,
+        logging.WARNING: defaultFormat,
+        logging.ERROR: defaultFormat,
+        logging.CRITICAL: defaultFormat
+    }
+
+    def format(self, record):
+        logFormat = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(logFormat, datefmt=self.timeFormat)
+
+        return formatter.format(record)
 
 def setup_logger(name):
 
@@ -10,14 +30,15 @@ def setup_logger(name):
 
     # Add [from %(funcName)s in %(module)s] in debug level (specify a formatter for differents level)
 
-    formatter = logging.Formatter('[%(asctime)s]-[%(levelname)s] : %(message)s', datefmt='%H:%M:%S')
+    #formatter = logging.Formatter('[%(asctime)s]-[%(levelname)s] : %(message)s', datefmt='%H:%M:%S')
 
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
 
     Consolehandler = logging.StreamHandler(sys.stdout)
-    Consolehandler.setFormatter(formatter)
+    #Consolehandler.setFormatter(formatter)
+    Consolehandler.setFormatter(CustomFormatter())
     logger.addHandler(Consolehandler)
 
 
