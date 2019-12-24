df334417b49cf8249f4550bfb3d9fdf597f365fe
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-12-24 16:36:09 -0600

    Add core.channels modules, to manage sockets in new file, with proxy

---
+++ core/channels.py
@@ -0,0 +1,24 @@
+import socks
+import logging
+
+
+
+def bind_tcp(ip, port, proxy):
+    """Create a TCP socket with binding remote agent"""
+
+
+    logger = logging.getLogger('main')
+
+
+    logger.debug(f"Remote agent IP : {ip}")
+    logger.debug(f"Remote agent port : {port}")
+    logger.debug(f"Proxy : {proxy}")
+
+
+
+
+
+
+
+    return
+
