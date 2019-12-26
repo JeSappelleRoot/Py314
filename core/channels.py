2cb6200999c2ae9931717e09d3df91542c311fe6
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-12-26 10:53:52 -0600

    Add bind_tcp function and parseProxy in channels module, socks4/socks5 and HTTP proxy are availables

--- core/channels.py
+++ core/channels.py
@@ -1,5 +1,51 @@
+import socket
 import socks
 import logging
+
+def parseProxy(proxy):
+    """Function to parse proxy with following syntax <type>://<address>:<port>"""
+
+    try:
+
+        logger = logging.getLogger('main')
+
+        proxyType = proxy.split('://')[0]
+        proxyAddress = proxy.split('://')[1]
+        proxyIP = proxyAddress.split(':')[0]
+        proxyPort = proxyAddress.split(':')[1]
+        validProxy = True
+
+        availableProxies = ['SOCKS4', 'SOCKS5', 'HTTP']
+        if proxyType.upper() not in availableProxies:
+            logger.warning('Please use a valid proxy type : socks4, socks5 or http')
+            validProxy = False
+
+        if proxyType.upper() == 'SOCKS4':
+            proxyNum = 1
+        elif proxyType.upper() == 'SOCKS5':
+            proxyNum = 2
+        elif proxyType.upper() == 'HTTP':
+            proxyNum = 3
+        else:
+            proxyNum = 'UNVALID'
+
+        logger.debug(f"Proxy type : {proxyType} (type = {proxyNum})")
+        logger.debug(f"Proxy IP address : {proxyIP}")
+        logger.debug(f"Proxy port : {proxyPort}")
+
+        proxyParam = [proxyNum, proxyIP, int(proxyPort)]
+
+
+
+        return validProxy, proxyParam
+
+    except IndexError as error:
+        logger.warning('Please specify a proxy with <type>://<ip>:<port>')
+        proxyParam = False
+        validProxy = False
+
+        return validProxy, proxyParam
+
 
 
 
@@ -10,15 +56,50 @@ def bind_tcp(ip, port, proxy):
     logger = logging.getLogger('main')
 
 
-    logger.debug(f"Remote agent IP : {ip}")
+    logger.debug(f"Remote agent IP address : {ip}")
     logger.debug(f"Remote agent port : {port}")
-    logger.debug(f"Proxy : {proxy}")
+
+    channel = socks.socksocket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    channel.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    if proxy != '':
+        logger.debug('Proxy option detected, try to parse proxy')
+        valid, proxyOpt = parseProxy(proxy)
+
+        if valid is True and len(proxyOpt) == 3:
+
+            logger.debug('Proxy is valid, will be use for connection')
+            channel.set_proxy(proxy_type=proxyOpt[0], addr=proxyOpt[1], port=proxyOpt[2])
+        else:
+            return False
+
+
+    try:
+
+        logger.info(f"Try to established connection to {ip}:{port}")
+        channel.connect((ip, port))
+        logger.info(f"Channel successfully established with remote agent")
+
+    
+    except socks.ProxyConnectionError as error:
+        logger.warning(f"Can't established connection with specified proxy")
+        channel = False
+
+    except socks.GeneralProxyError as error:
+        logger.warning(f"Can't established connection to {ip}:{port}")
+        logger.debug('General proxy error')
+        channel = False
+
+
+    except ConnectionError as error:
+        logger.warning(f"Can't established connection to {ip}:{port}")
+        logger.debug(error)
+        channel = False
 
 
 
+    return channel
+    
 
 
-
-
-    return
 
