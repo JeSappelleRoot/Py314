e1fc5a0f25a2db18467f797559b9896208dabe5f
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-12-26 19:47:28 -0600

    Add some comments

--- core/logger.py
+++ core/logger.py
@@ -26,39 +26,46 @@ class CustomFormatter(logging.Formatter):
 
 
 def setup_logger(name, level):
+    """Define a logger with name and level"""
 
-    #level = logging.INFO
-
+    # Define a time format
     timeFormat = '%H:%M:%S'
 
+    # If level is logging.INFO, use short formatter
     if level == 20:
         formatter = logging.Formatter(
             '[%(asctime)s]-[%(levelname)s] : %(message)s', 
             datefmt=timeFormat
             )
+    # Else, use more verbose formatter by default
     else:
         formatter = logging.Formatter(
             '[%(asctime)s]-[%(levelname)s] (from %(funcName)s in %(module)s) : %(message)s', 
             datefmt=timeFormat
             )
 
+    # Add numeric values of news logging level
     SUCCESS, FAILURE, ASK = 21, 22, 23
+    # Define new levels, with headers, and colors ;)
     logging.addLevelName(SUCCESS, colored('+', 'green'))
     logging.addLevelName(FAILURE, colored('!', 'red',attrs=['blink']))
     logging.addLevelName(ASK, colored('?', 'yellow'))
 
+    # Define a logger with given name in argument and set level
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
+    # Add attributes to the current logger, tu use new levels
     setattr(logger, 'success', lambda *args: logger.log(SUCCESS, *args))
     setattr(logger, 'failure', lambda *args: logger.log(FAILURE, *args))
     setattr(logger, 'ask', lambda *args: logger.log(ASK, *args))
 
+    # Define a console handler, to write output to the console
     Consolehandler = logging.StreamHandler(sys.stdout)
-
-    
+    # Set the formatter
     Consolehandler.setFormatter(formatter)
 
+    # Finally add handler
     logger.addHandler(Consolehandler)
 
 
