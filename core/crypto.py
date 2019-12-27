9810c500eff272cbf2977064e6ff2832a0a84353
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-12-26 19:54:52 -0600

    Add some comments in crypto module

--- core/crypto.py
+++ core/crypto.py
@@ -14,10 +14,14 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 def generate_key(password):
     """Generate fernet key from password"""
 
+    # Get main logger
     logger = logging.getLogger('main')
 
+    # Create a string hasher with SHA256 algoritm
     digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+    # Add password to hasher 
     digest.update(password.encode())
+    # Generate Fernet key from the SHA256 digest of password
     key = base64.urlsafe_b64encode(digest.finalize())
 
     logger.debug(f"Password to generate key : {password}")
@@ -30,14 +34,18 @@ def encrypt_file(password, infile, outfile):
 
     try:
 
+        # Get main logger
         logger = logging.getLogger('main')
+        # Create fernet object, with key from given password
         fernet = Fernet(generate_key(password))
 
+        # Open source plain file in binary read mode and get content
         with open(infile, 'rb') as fileStream:
             plainData = fileStream.read()
 
+        # Open output file in binary creation mode
         with open(outfile, 'xb') as fileStream:
-
+            # Encrypt plain content of source file and write it to the destination file
             encrypted = fernet.encrypt(plainData)
             fileStream.write(encrypted)
 
@@ -53,13 +61,17 @@ def encrypt_message(password, message):
     
     try:
 
+        # Get main logger
         logger = logging.getLogger('main')
+        # Create fernet object with key from password
         fernet = Fernet(generate_key(password))
 
+        # Encode message (str to bytes)
         message = message.encode()
+        # And encrypt this message with Fernet
         encrypted = fernet.encrypt(message)
 
-        logger.debug(f"Message before encryption : {message}")
+        logger.debug(f"Message before encryption : {message.decode()}")
         logger.debug(f"Message after encryption : {encrypted.decode()}")
 
         return encrypted.decode()
@@ -74,15 +86,19 @@ def decrypt_file(password, infile, outfile):
 
     try:
 
+        # Get main logger
         logger = logging.getLogger('main')
+        # Create fernet object with key from password
         fernet = Fernet(generate_key(password))
 
+        # Open encrypted source file in binary read mode
         with open(infile, 'rb') as fileStream:
             cipherData = fileStream.read()
-
+        # Open plain destination ffile in write binary mode
         with open(outfile, 'wb') as fileStream:
-
+            # Decrypt the encrypted content with fernet
             decrypted = fernet.decrypt(cipherData)
+            # And write it to output file
             fileStream.write(decrypted)
 
         logger.debug(f"Source file for decryption : {infile}")
@@ -97,13 +113,16 @@ def decrypt_message(password, message):
 
     try:
 
+        # Get the main logger
         logger = logging.getLogger('main')
+        # Create a fernet object with key from password
         fernet = Fernet(generate_key(password))
-
+        # Encode message (str to bytes)
         message = message.encode()
+        # Decrypted encrypted message with the fernet object
         decrypted = fernet.decrypt(message)
 
-        logger.debug(f"Message before decryption : {message}")
+        logger.debug(f"Message before decryption : {message.decode()}")
         logger.debug(f"Message after decryption : {decrypted.decode()}")
 
         return decrypted.decode()
