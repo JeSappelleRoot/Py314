949d800a429456386b1117c0392c8dabe95ae079
JeSappelleRoot <joseph.morin.it@gmail.com>
2019-12-30 13:42:11 -0600

    Remove unused command in main interpreter

--- Py314.py
+++ Py314.py
@@ -100,9 +100,6 @@ class Prompt(Cmd):
         """Try to established a connection with a Py314 agent"""
         handler.startModule()
 
-    def do_reverse_agent(self, arg):
-        """Try to established a connection with listening a Py314 agent"""
-
     def do_factory(self, arg):
         """Generate a Py314 agent"""
         factory.startFactory()
